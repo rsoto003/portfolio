ed3ec6be8f688df2197f1926d0cbe1292d80e569
Ryan Soto <rsoto003@ucr.edu>
2018-07-09 00:09:13 -0700

    editing passwords for mailer

--- dist/php_mailer/email_config.php
+++ dist/php_mailer/email_config.php
@@ -1,6 +1,6 @@
 <?php
 define('EMAIL_USER','contactryansotodeveloper@gmail.com');
 define('EMAIL_USERNAME', 'Ryan Soto');
-define('EMAIL_PASS','Rps04trojans');
+define('EMAIL_PASS','Rps04trojans/04');
 define('EMAIL_TO_ADDRESS'. 'ryansotodev@gmail.com');
 ?>
